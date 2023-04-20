566efb0cce33a00b5ceb3094f42b1674c3e4ee53
Ana Arellano <117821773+acarecor@users.noreply.github.com>
2023-04-20 12:19:33 +0200

    added a pokemon list
    If applied, this commit will have added a pokemon list.

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,6 @@
-alert('Hello world');
-let favoriteFood = 'ceviche';
-document.write(favoriteFood);
+//Adding a Pokemon List 
+let pokemonList = [
+  {name:"Dragonite", height:7, type:['dragon','flying']},
+  {name:"Amaura", height:4 , type:['rock','ice']},
+  {name:"Oricorio", height:2 , type:['fire','flying']}
+];
