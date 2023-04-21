c48b318bc1490b5aeff9383eea8e85015e7723ba
Ana Arellano <117821773+acarecor@users.noreply.github.com>
2023-04-21 20:06:14 +0200

    created a for loop for the pokemon list
    If applied, this commit will have created a for loop for the pokemon list.

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,16 @@
 //Adding a Pokemon List 
 let pokemonList = [
-  {name:"Dragonite", height:7, type:['dragon','flying']},
-  {name:"Amaura", height:4 , type:['rock','ice']},
-  {name:"Oricorio", height:2 , type:['fire','flying']}
+  { name: "Dragonite", height: 7, type: ['dragon', 'flying'] },
+  { name: "Amaura", height: 4, type: ['rock', 'ice'] },
+  { name: "Oricorio", height: 2, type: ['fire', 'flying'] }
 ];
+
+//  for loop that iterates over each element in the pokemon list:
+for (let i = 0; i < pokemonList.length; i++) {
+  if (pokemonList[i].height < 10 && pokemonList[i].height > 5) {
+    document.write(`<p> ${pokemonList[i].name} (height: ${pokemonList[i].height}) - Wow that's big! </p>`);
+  }
+  else {
+    document.write(`<p>${pokemonList[i].name} (height: ${pokemonList[i].height}) </p>`);
+  }
+}
