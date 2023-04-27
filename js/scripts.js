18c07a328e7d36b24a742745a8b6ee74d2fc0942
Ana Arellano <117821773+acarecor@users.noreply.github.com>
2023-04-27 10:56:21 +0200

    created pokemonRepository containing IIFE pokemonList
    If applied, this commit will have created a pokemonRepository containing IIFE  :  pokemonList and also two new functions: add and getAll.
    also the for loop was upgraded to forEach loop

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,43 @@
-//Adding a Pokemon List 
-let pokemonList = [
-  { name: "Dragonite", height: 7, type: ['dragon', 'flying'] },
-  { name: "Amaura", height: 4, type: ['rock', 'ice'] },
-  { name: "Oricorio", height: 2, type: ['fire', 'flying'] }
-];
+// new pokemonRepository variable to hold the IIFE with the pokemonList
+let pokemonRepository = (function(){
 
-//  for loop that iterates over each element in the pokemon list:
-for (let i = 0; i < pokemonList.length; i++) {
-  if (pokemonList[i].height < 10 && pokemonList[i].height > 5) {
-    document.write(`<p> ${pokemonList[i].name} (height: ${pokemonList[i].height}) - Wow that's big! </p>`);
+//Adding a Pokemon List 
+  let pokemonList = [
+    { name: "Dragonite", height: 7, type: ['dragon', 'flying'] },
+    { name: "Amaura", height: 4, type: ['rock', 'ice'] },
+    { name: "Oricorio", height: 2, type: ['fire', 'flying'] }
+  ];
+// functions inside the pokemon repository:
+//getAll: return all items (pokemonRepository.getAll(); should return the pokemonList array)
+  function getAll(){
+    return pokemonList; 
+  }
+  //add: add a single item to the pokemonList array 
+  function add(item) {
+    pokemonList.push(item)
+  } 
+
+  return{
+    add,
+    getAll
+  };
+
+})();
+
+//calling pokemonRepository.add(item); should add the Pokémon referred to with item to the pokemonList array)
+pokemonRepository.add({name:'Pikachu', height:1.04, type:['electric']});
+console.log(pokemonRepository.getAll());
+
+// conditional function of height of each pokemon
+function pokemonHeightCondition(item) {
+  if (item.height < 10 && item.height > 5) {
+    document.write(`<p> ${item.name} (height: ${item.height}) - Wow that's big! </p>`);
   }
   else {
-    document.write(`<p>${pokemonList[i].name} (height: ${pokemonList[i].height}) </p>`);
+    document.write(`<p>${item.name} (height: ${item.height}) </p>`);
   }
 }
+
+//  forEach loop that iterates over each element in the pokemonList inside the repository and use the function pokemonHeightCondition:
+
+(pokemonRepository.getAll()).forEach (pokemonHeightCondition); 
